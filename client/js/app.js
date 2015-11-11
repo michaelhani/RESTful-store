f545939e57a116063f573d44a3edca0b83269660
Michael J. Hanifan <michael.hanifan@gmail.com>
2015-11-10 16:36:34 -0800

    improved image display for products page

--- client/js/app.js
+++ client/js/app.js
@@ -16,3 +16,11 @@ app.config(function($routeProvider){
 	})
 	.otherwise('/');
 });
+
+app.filter('capitalize', function() {
+  return function(input, scope) {
+    if (input!=null)
+    input = input.toLowerCase();
+    return input.substring(0,1).toUpperCase()+input.substring(1);
+  }
+});
