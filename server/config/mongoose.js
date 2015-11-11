360e0b95cc058045065ed79cb0d0721dc9978398
Michael J. Hanifan <michael.hanifan@gmail.com>
2015-11-10 16:09:04 -0800

    minor fixes for heroku

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var keys = require(__dirname+"/keys.js");
 
 mongoose.connect(keys.remote_db);
-var models_path=__dirname+'./../models';
+var models_path=__dirname+'/../models';
 fs.readdirSync(models_path).forEach(function(file){
 	if(file.indexOf('.js')>=0){
 		require(models_path+'/'+file);
